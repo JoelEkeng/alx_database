e8b574fee22c5d0afb96c9480a468477e448b367
Joel Ekeng <joelekeng23@gmail.com>
2023-08-15 18:01:52 +0000

    Creating a table

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
---Create a table name firs-name
+-- creates a table if not exist with columns.
 CREATE TABLE IF NOT EXISTS first_table (
-    id INT,
-    name VARCHAR(256)
+  id INT,
+  name VARCHAR(256)
 );
