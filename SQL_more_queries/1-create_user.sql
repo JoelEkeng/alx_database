d001e3e94db632489f858eafe9f8f37760f93292
Joel Ekeng <joelekeng23@gmail.com>
2023-08-17 12:14:27 +0000

    Creates users with all privilages

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- Script that creates users
 CREATE IF NOT EXISTS USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd'
-GRANT ALL PRIVILEGES *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
