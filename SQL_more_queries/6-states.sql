0ca62b514046e7abd4e328dc58848f730873e32a
Joel Ekeng <joelekeng23@gmail.com>
2023-08-17 12:47:00 +0000

    States with a UNIQUE attributes

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,5 +1,8 @@
 -- Script that creates database and Table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
 CREATE TABLE IF NOT EXISTS states(
     id INT UNIQUE AUTO_INCREMENT PRIMARY KEY NOT NULL,
     name VARCHAR(256)
