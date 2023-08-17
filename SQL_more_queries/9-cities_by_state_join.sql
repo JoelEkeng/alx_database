5425dc2883da2ab935b1371322646d29ea4c3ef8
Joel Ekeng <joelekeng23@gmail.com>
2023-08-17 13:18:19 +0000

    Using joins on citites

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- Script that lists all cities from hbtn_0d_usa database.
+SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC
