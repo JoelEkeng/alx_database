34c0eedbd43ee189b19c42a71f37db87e84a2b69
Joel Ekeng <joelekeng23@gmail.com>
2023-08-17 12:57:23 +0000

    Cities with a Foreign Key and other attributes

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,5 +1,8 @@
 -- Script that creates a database and table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_2;
+
 CREATE TABLE IF NOT EXISTS cities(
     id INT UNIQUE AUTO_INCREMENT PRIMARY KEY NOT NULL,
     state_id INT NOT NULL FOREIGN KEY(state_id) REFERENCES states(id),
